cd81420b550bb0f83f7b3d75b5db45cd6cc07e75
BayuDC <dev.bayudc@gmail.com>
2024-01-07 14:28:59 +0700

    chore: initial setup

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
+
+class AppServiceProvider extends ServiceProvider {
+    /**
+     * Register any application services.
+     */
+    public function register(): void {
+        Sanctum::ignoreMigrations();
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void {
+        //
+    }
+}
