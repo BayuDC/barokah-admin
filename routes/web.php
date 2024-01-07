6f8064295472e0eb6ef65681f347bc33d9049c55
BayuDC <dev.bayudc@gmail.com>
2024-01-07 15:03:45 +0700

    chore: setup notus template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
+});
+Route::get('/admin', function () {
+    return view('admin/dashboard');
 });
