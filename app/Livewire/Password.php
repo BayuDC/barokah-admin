6c143ca5f02eaff6e0ffa603daaa5352f2e3f892
BayuDC <dev.bayudc@gmail.com>
2024-01-26 14:11:50 +0700

    feat: change user password

--- app/Livewire/Password.php
+++ app/Livewire/Password.php
@@ -2,12 +2,33 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Password extends Component {
 
-    #[Title('Ganti Kata Sandi')]
+    #[Validate('required', message: 'Kata sandi lama harus diisi')]
+    #[Validate('current_password', message: 'Kata sandi lama tidak benar')]
+    public $passwordOld;
+
+    #[Validate('required', message: 'Kata sandi baru harus diisi')]
+    #[Validate('min:8', message: 'Panjang kata sandi minimal 8 karakter')]
+    public $passwordNew;
+
+    public function save() {
+        $body = $this->validate();
+
+        $user = Auth::user();
+        $user->password = Hash::make($body['passwordNew']);
+        $user->save();
+
+        return redirect()->to('/admin/password')->with('message', 'Kata sandi berhasil diganti');
+    }
+
+    #[Title('Kata Sandi')]
     public function render() {
         return view('livewire.password');
     }
--- resources/views/livewire/password.blade.php
+++ resources/views/livewire/password.blade.php
@@ -1,8 +1,8 @@
 <x-section title="Ganti Kata Sandi">
     <x-form>
         <x-form-group>
-            <x-input-text label="Kata Sandi Lama" model="" type="password" />
-            <x-input-text label="Kata Sandi Baru" model="" type="password" />
+            <x-input-text label="Kata Sandi Lama" model="passwordOld" type="password" />
+            <x-input-text label="Kata Sandi Baru" model="passwordNew" type="password" />
         </x-form-group>
         <x-form-divider />
         <x-form-control simple />
