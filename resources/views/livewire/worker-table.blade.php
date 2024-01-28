8d857e0ff85fa95f77548988b99ac3e0d4a4c101
BayuDC <dev.bayudc@gmail.com>
2024-01-28 14:48:40 +0700

    refactor: polish the worker table

--- resources/views/livewire/worker-table.blade.php
+++ resources/views/livewire/worker-table.blade.php
@@ -8,14 +8,26 @@
             <tr>
                 <x-table-cell>{{ $user->id }}</x-table-cell>
                 <x-table-cell>
-                    <div class="h-12 w-12 aspect-square overflow-hidden rounded-full border border-gray-400">
+                    <div class="h-12 w-12 aspect-square overflow-hidden rounded-full border-2 border-indigo-600">
                         <img src="{{ $user['picture_url'] }}"
                             class=" bg-white w-full">
                     </div>
                 </x-table-cell>
                 <x-table-cell>{{ $user->name }}</x-table-cell>
                 <x-table-cell>{{ $user->email }}</x-table-cell>
-                <x-table-cell>{{ $user->role == 'admin' ? 'Admin' : 'Karyawan' }}</x-table-cell>
+                <x-table-cell>
+                    @if ($user->role == 'admin')
+                        <span
+                            class="text-xs font-semibold inline-block py-1 px-2 rounded text-purple-600 bg-purple-200 uppercase">
+                            Admin
+                        </span>
+                    @else
+                        <span
+                            class="text-xs font-semibold inline-block py-1 px-2 rounded text-teal-600 bg-teal-200 uppercase">
+                            Karyawan
+                        </span>
+                    @endif
+                </x-table-cell>
                 <x-table-cell>
                     <x-button-detail href="/admin/workers/{{ $user->id }}" />
                     <x-button-update href="/admin/workers/update/{{ $user->id }}" />
