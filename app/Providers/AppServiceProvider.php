5224915c5667baed700dd7d9913a5fb1260ff6ee
BayuDC <dev.bayudc@gmail.com>
2024-01-29 19:27:51 +0700

    fix: production proxy issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,8 @@ class AppServiceProvider extends ServiceProvider {
      * Bootstrap any application services.
      */
     public function boot(): void {
-        //
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
