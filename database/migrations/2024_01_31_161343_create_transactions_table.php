69033e7d637339ced8f57bade8cee282d23fde00
BayuDC <dev.bayudc@gmail.com>
2024-02-01 14:10:05 +0700

    feat: transaction model

---
+++ database/migrations/2024_01_31_161343_create_transactions_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->enum('status', ['created', 'confirmed', 'canceled', 'finished'])->nullable();
+            $table->enum('payment', ['cash', 'debit']);
+            $table->unsignedBigInteger('final_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists('transactions');
+    }
+};
